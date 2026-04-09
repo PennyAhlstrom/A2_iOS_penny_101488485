299469b05ac67f12e7e151087243f56cea721b40
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-04-09 13:01:50 -0400

    add product detail view controller with first item display and navigation

---
+++ A2_iOS_penny_101488485/ProductDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductDetailViewController.swift
+//  A2_iOS_penny_101488485
+//
+//  Created by Penny Ahlstrom on 2026-04-09.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
